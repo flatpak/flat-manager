10c96e619b8cacf8623c5ffa8d72b606f844cae1
Alexander Larsson <alexl@redhat.com>
2018-10-23 12:41:04 +0200

    Break out app to app.rs

---
+++ src/app.rs
@@ -0,0 +1,58 @@
+use actix::prelude::*;
+use actix_web::{self, fs, middleware};
+use actix_web::{App, http::Method, HttpRequest, fs::NamedFile};
+use models::DbExecutor;
+use std::path::PathBuf;
+use std::path::Path;
+use std::ffi::OsString;
+
+use api::create_build;
+use api::get_build;
+use api::create_build_ref;
+use api::upload;
+use api::missing_objects;
+
+pub struct AppState {
+    pub db: Addr<DbExecutor>,
+    pub repo_path: PathBuf,
+    pub build_repo_base_path: PathBuf,
+}
+
+fn handle_build_repo(req: &HttpRequest<AppState>) -> actix_web::Result<NamedFile> {
+    let tail: String = req.match_info().query("tail")?;
+    let id: String = req.match_info().query("id")?;
+    let state = req.state();
+    let path = Path::new(&state.build_repo_base_path).join(&id).join(tail.trim_left_matches('/'));
+    println!("Handle build repo {:?}", path);
+    NamedFile::open(path).or_else(|_e| {
+        let fallback_path = Path::new(&state.repo_path).join(tail.trim_left_matches('/'));
+        Ok(NamedFile::open(fallback_path)?)
+    })
+}
+
+pub fn create_app(
+    db: Addr<DbExecutor>,
+    repo_path: &OsString,
+    build_repo_base_path: &OsString,
+) -> App<AppState> {
+    let state = AppState {
+        db: db.clone(),
+        repo_path: PathBuf::from(repo_path),
+        build_repo_base_path: PathBuf::from(build_repo_base_path),
+    };
+
+    let repo_static_files = fs::StaticFiles::new(&state.repo_path)
+        .expect("failed constructing repo handler");
+
+    App::with_state(state)
+        .middleware(middleware::Logger::default())
+        .resource("/api/v1/build", |r| r.method(Method::POST).with(create_build))
+        .resource("/api/v1/build/{id}", |r| r.method(Method::GET).with(get_build))
+        .resource("/api/v1/build/{id}/refs", |r| r.method(Method::POST).with(create_build_ref))
+        .resource("/api/v1/build/{id}/upload", |r| r.method(Method::POST).with(upload))
+        .resource("/api/v1/build/{id}/missing_objects", |r| r.method(Method::GET).with(missing_objects))
+        .scope("/build-repo/{id}", |scope| {
+            scope.handler("/", |req: &HttpRequest<AppState>| handle_build_repo(req))
+        })
+        .handler("/repo", repo_static_files)
+}
--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,8 @@ extern crate serde_json;
 extern crate tempfile;
 
 use actix::prelude::*;
-use actix_web::{dev, error, fs, http, middleware, multipart, server,};
-use actix_web::{App, AsyncResponder, FutureResponse, HttpMessage, HttpRequest, HttpResponse, Json, Path, Result, State,};
+use actix_web::{dev, error, multipart, server,};
+use actix_web::{AsyncResponder, FutureResponse, HttpMessage, HttpRequest, HttpResponse, Json, Path, Result, State,};
 use actix_web::error::{ErrorBadRequest,};
 
 use diesel::prelude::*;
@@ -33,250 +33,16 @@ use std::rc::Rc;
 use std::cell::RefCell;
 use tempfile::NamedTempFile;
 
-
+mod api;
 mod db;
 mod models;
 mod schema;
 mod errors;
+mod app;
 
+use app::{AppState};
 use db::{CreateBuild, CreateBuildRef, LookupBuild};
 use models::{DbExecutor, NewBuildRef};
-
-struct AppState {
-    db: Addr<DbExecutor>,
-    repo_path: path::PathBuf,
-    build_repo_base_path: path::PathBuf,
-}
-
-fn create_build(
-    state: State<AppState>,
-) -> FutureResponse<HttpResponse> {
-    state
-        .db
-        .send(CreateBuild { })
-        .from_err()
-        .and_then(|res| match res {
-            Ok(build) => Ok(HttpResponse::Ok().json(build.id)),
-            Err(_) => Ok(HttpResponse::InternalServerError().into()),
-        })
-        .responder()
-}
-
-#[derive(Deserialize)]
-pub struct BuildPathParams {
-    id: i32,
-}
-
-fn get_build(
-    (params, state): (Path<BuildPathParams>, State<AppState>),
-) -> FutureResponse<HttpResponse> {
-    state
-        .db
-        .send(LookupBuild { id: params.id })
-        .from_err()
-        .and_then(|res| match res {
-            Ok(build) => Ok(HttpResponse::Ok().json(build)),
-            Err(_) => Ok(HttpResponse::InternalServerError().into()),
-        })
-        .responder()
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct MissingObjectsArgs {
-    wanted: Vec<String>
-}
-
-fn has_object (build_id: i32, object: &str, state: &State<AppState>) -> bool
-{
-    let subpath: path::PathBuf = ["objects", &object[..2], &object[2..]].iter().collect();
-    let build_path = state.build_repo_base_path.join(build_id.to_string()).join(&subpath);
-    if build_path.exists() {
-        true
-    } else {
-        let main_path = state.repo_path.join(&subpath);
-        main_path.exists()
-    }
-}
-
-fn missing_objects(
-    (missing_objects, params, state): (Json<MissingObjectsArgs>, Path<BuildPathParams>, State<AppState>),
-) -> HttpResponse {
-    let mut missing = vec![];
-    for object in &missing_objects.wanted {
-        if ! has_object (params.id, object, &state) {
-            missing.push(object);
-        }
-    }
-    println!("{}: {:?}", params.id, &missing);
-    HttpResponse::Ok().json(&missing)
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CreateBuildRefArgs {
-    #[serde(rename = "ref")] ref_name: String,
-    commit: String,
-}
-
-fn create_build_ref (
-    (args, params, state): (Json<CreateBuildRefArgs>, Path<BuildPathParams>, State<AppState>),
-) -> FutureResponse<HttpResponse> {
-    println!("{}: {:?}", params.id, &args);
-    state
-        .db
-        .send(CreateBuildRef {
-            data: NewBuildRef {
-                build_id: params.id,
-                ref_name: args.ref_name.clone(),
-                commit: args.commit.clone(),
-            }
-        })
-        .from_err()
-        .and_then(|res| match res {
-            Ok(buildref) => Ok(HttpResponse::Ok().json(buildref.id)),
-            Err(_) => Ok(HttpResponse::InternalServerError().into()),
-        })
-        .responder()
-}
-
-fn handle_build_repo(req: &HttpRequest<AppState>) -> Result<fs::NamedFile> {
-    let tail: String = req.match_info().query("tail")?;
-    let id: String = req.match_info().query("id")?;
-    let state = req.state();
-    let path = path::Path::new(&state.build_repo_base_path).join(&id).join(tail.trim_left_matches('/'));
-    println!("Handle build repo {:?}", path);
-    fs::NamedFile::open(path).or_else(|_e| {
-        let fallback_path = path::Path::new(&state.repo_path).join(tail.trim_left_matches('/'));
-        Ok(fs::NamedFile::open(fallback_path)?)
-    })
-}
-
-fn objectname_is_valid(name: &str) -> bool {
-    let v: Vec<&str> = name.splitn(2, ".").collect();
-
-    v.len() == 2 &&
-        v[0].len() == 64  &&
-        !v[0].contains(|c: char| !(c.is_digit(16) && !c.is_uppercase())) &&
-        (v[1] == "dirmeta" ||
-         v[1] == "dirtree" ||
-         v[1] == "filez" ||
-         v[1] == "commit")
-}
-
-fn get_object_name(field: &multipart::Field<dev::Payload>) -> error::Result<String, io::Error> {
-    let cd = field.content_disposition().ok_or(
-        io::Error::new(io::ErrorKind::InvalidInput,
-                       "No content disposition for multipart item"))?;
-    let filename = cd.get_filename().ok_or(
-        io::Error::new(io::ErrorKind::InvalidInput,
-                       "No filename for multipart item"))?;
-    if !objectname_is_valid(filename) {
-        Err(io::Error::new(io::ErrorKind::InvalidInput,
-                           "Invalid object name"))
-    } else {
-        Ok(filename.to_string())
-    }
-}
-
-struct UploadState {
-    repo_path: path::PathBuf,
-}
-
-fn start_save(
-    object: String,
-    state: &Arc<UploadState>,
-) -> Result<(NamedTempFile,path::PathBuf)> {
-    let objects_dir = state.repo_path.join("objects");
-    let object_dir = objects_dir.join(&object[..2]);
-    let object_file = object_dir.join(&object[2..]);
-
-    std::fs::create_dir_all(object_dir)?;
-
-    let tmp_dir = state.repo_path.join("tmp");
-    std::fs::create_dir_all(&tmp_dir)?;
-
-    let named_file = NamedTempFile::new_in(&tmp_dir)?;
-    Ok((named_file, object_file))
-}
-
-fn save_file(
-    field: multipart::Field<dev::Payload>,
-    state: &Arc<UploadState>
-) -> Box<Future<Item = i64, Error = error::Error>> {
-    let object = match  get_object_name (&field) {
-        Ok(name) => name,
-        Err(e) => return Box::new(future::err(ErrorBadRequest(e))),
-    };
-
-    let (named_file, object_file) = match start_save (object, state) {
-        Ok((named_file, object_file)) => (named_file, object_file),
-        Err(e) => return Box::new(future::err(error::ErrorInternalServerError(e))),
-    };
-
-    // We need file in two continuations below, so put it in a Rc+RefCell
-    let shared_file = Rc::new(RefCell::new(named_file));
-    let shared_file2 = shared_file.clone();
-    Box::new(
-        field
-            .fold(0i64, move |acc, bytes| {
-                let rt = shared_file.borrow_mut()
-                    .write_all(bytes.as_ref())
-                    .map(|_| acc + bytes.len() as i64)
-                    .map_err(|e| {
-                        println!("file.write_all failed: {:?}", e);
-                        error::MultipartError::Payload(error::PayloadError::Io(e))
-                    });
-                future::result(rt)
-            })
-            .map_err(|e| {
-                println!("save_file failed, {:?}", e);
-                error::ErrorInternalServerError(e)
-            }).and_then (move |res| {
-                // persist consumes the named file, so we need to
-                // completely move it out of the shared Rc+RefCell
-                let named_file = Rc::try_unwrap(shared_file2).unwrap().into_inner();
-                match named_file.persist(object_file) {
-                    Ok(_persisted_file) => future::result(Ok(res)),
-                    Err(e) => future::err(error::ErrorInternalServerError(e))
-                }
-            }),
-    )
-}
-
-fn handle_multipart_item(
-    item: multipart::MultipartItem<dev::Payload>,
-    state: &Arc<UploadState>
-) -> Box<Stream<Item = i64, Error = error::Error>> {
-    match item {
-        multipart::MultipartItem::Field(field) => {
-            Box::new(save_file(field, state).into_stream())
-        }
-        multipart::MultipartItem::Nested(mp) => {
-            let s = state.clone();
-            Box::new(mp.map_err(error::ErrorInternalServerError)
-                     .map(move |item| { handle_multipart_item (item, &s) })
-                     .flatten())
-        }
-    }
-}
-
-fn upload(
-    (params, req): (Path<BuildPathParams>, HttpRequest<AppState>)
-) -> FutureResponse<HttpResponse> {
-    let state = req.state();
-    let uploadstate = Arc::new(UploadState { repo_path: state.build_repo_base_path.join(params.id.to_string()) });
-    Box::new(
-        req.multipart()
-            .map_err(error::ErrorInternalServerError)
-            .map(move |item| { handle_multipart_item (item, &uploadstate) })
-            .flatten()
-            .collect()
-            .map(|sizes| HttpResponse::Ok().json(sizes))
-            .map_err(|e| {
-                println!("failed: {}", e);
-                e
-            }),
-    )
-}
 
 fn main() {
     ::std::env::set_var("RUST_LOG", "actix_web=info");
@@ -299,28 +65,8 @@ fn main() {
 
     let addr = SyncArbiter::start(3, move || DbExecutor(pool.clone()));
 
-
     server::new(move || {
-        let state = AppState {
-            db: addr.clone(),
-            repo_path: path::PathBuf::from(&repo_path),
-            build_repo_base_path: path::PathBuf::from(&build_repo_base_path),
-        };
-
-        let repo_static_files = fs::StaticFiles::new(&state.repo_path)
-            .expect("failed constructing repo handler");
-
-        App::with_state(state)
-            .middleware(middleware::Logger::default())
-            .resource("/api/v1/build", |r| r.method(http::Method::POST).with(create_build))
-            .resource("/api/v1/build/{id}", |r| r.method(http::Method::GET).with(get_build))
-            .resource("/api/v1/build/{id}/refs", |r| r.method(http::Method::POST).with(create_build_ref))
-            .resource("/api/v1/build/{id}/upload", |r| r.method(http::Method::POST).with(upload))
-            .resource("/api/v1/build/{id}/missing_objects", |r| r.method(http::Method::GET).with(missing_objects))
-            .scope("/build-repo/{id}", |scope| {
-                scope.handler("/", |req: &HttpRequest<AppState>| handle_build_repo(req))
-            })
-            .handler("/repo", repo_static_files)
+        app::create_app(addr.clone(), &repo_path, &build_repo_base_path)
     }).bind("127.0.0.1:8080")
         .unwrap()
         .start();
