aa677036c9a3f58da9443e3d42115d92ab55e5b8
Alexander Larsson <alexl@redhat.com>
2018-10-23 16:55:45 +0200

    errors: Add From<DieselError>

--- src/errors.rs
+++ src/errors.rs
@@ -1,21 +1,29 @@
 use actix_web::{error::ResponseError, HttpResponse};
+use diesel::result::{Error as DieselError};
 
 #[derive(Fail, Debug)]
 pub enum ApiError {
     #[fail(display = "Internal Server Error")]
     InternalServerError,
 
-    #[fail(display = "BadRequest: {}", _0)]
-    BadRequest(String),
+    #[fail(display = "NotFound")]
+    NotFound,
+}
+
+impl From<DieselError> for ApiError {
+    fn from(e: DieselError) -> Self {
+        match e {
+            DieselError::NotFound => ApiError::NotFound,
+            _ => ApiError::InternalServerError
+        }
+    }
 }
 
 impl ResponseError for ApiError {
     fn error_response(&self) -> HttpResponse {
         match *self {
-            ApiError::InternalServerError => {
-                HttpResponse::InternalServerError().json("Internal Server Error")
-            },
-            ApiError::BadRequest(ref message) => HttpResponse::BadRequest().json(message),
+            ApiError::InternalServerError => HttpResponse::InternalServerError().json("Internal Server Error"),
+            ApiError::NotFound => HttpResponse::NotFound().json("Not found"),
         }
     }
 }
